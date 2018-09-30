793380759321ff5194fc509acc9dcd24d067514c
Mark <41021707+markmalko@users.noreply.github.com>
2018-09-30 19:22:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency>10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }else if(currency<=0){
+        return {};
+    }else{
+    let half = 0;
+    let quarter = 0;
+    let dime = 0;
+    let nickel = 0;
+    let penny = 0;
+    while(currency>=50){
+        currency-=50;
+        half++;
+    }
+    while(currency>=25){
+        currency-=25;
+        quarter++;
+    }
+    while(currency>=10){
+        currency-=10;
+        dime++;
+    }
+    while(currency>=5){
+        currency-=5;
+        nickel++;
+    }
+    while(currency>0){
+        currency-=1;
+        penny++;
+    }
+    return {"H:":half, "Q:":quarter, "D:": dime, "N":nickel, "P:":penny};
+    }
 }
